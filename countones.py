bc5868af897ec3272ce25de872e7c87fc6de706e
Adrian Camacho Vazquez <at6689@c130-37.enst.fr>
2019-03-21 19:11:57 +0100

    features

---
+++ countones.py
@@ -0,0 +1,25 @@
+
+
+def percentageofones(listSQ):
+    
+    listPer=[]
+    for i in listSQ:
+        c = i.count("1")
+        p = c/12*100
+        listPer.append(p)        
+        print(listPer)
+    
+    return listPer
+
+
+def savedbits(listSQ,listCompressed):
+    
+    listbits=[]
+    
+    for i,j in zip(listSQ,listCompressed): 
+        
+        listbits.append(len(i)-len(j))
+        
+    return listbits
+    
+
